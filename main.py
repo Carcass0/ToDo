d1aa679cabd69c0fa93006d16d696a331f238c6c
Carcass <0carcass.00@gmail.com>
2024-02-19 17:45:50 +0300

    refactoring; work paused for now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QScrollArea, QMainWindow
+
 from taskbox import Ui_Form
 from mainwindow import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
 
@@ -9,53 +11,52 @@ class MainWindow(QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-app = QApplication([])
-main_widget = MainWindow()
-main_widget.setContentsMargins(0,0,0,0)
-scroll_widget = QWidget()
-scroll_layout = QVBoxLayout(scroll_widget)
-scroll_layout.setSpacing(0)
-scroll_layout.setContentsMargins(0,0,0,0)
-scroll_widget.setContentsMargins(0,0,0,0)
-for _ in range(5):
-    widget = QWidget()
-    ui = Ui_Form()
-    ui.setupUi(widget)
-    scroll_layout.addWidget(widget)
 
-scroll_area = QScrollArea()
-scroll_area.setWidget(scroll_widget)
-scroll_area.setWidgetResizable(True)
-scroll_area.setContentsMargins(0,0,0,0)
-scroll_area.setStyleSheet(
-    """
-    QScrollArea {
-    border: none;
-    }
+if __name__=="__main__":
+    app = QApplication([])
+    main_widget = MainWindow()
+    main_widget.setContentsMargins(0,0,0,0)
+    scroll_widget = QWidget()
+    scroll_layout = QVBoxLayout(scroll_widget)
+    scroll_layout.setSpacing(0)
+    scroll_layout.setContentsMargins(0,0,0,0)
+    scroll_widget.setContentsMargins(0,0,0,0)
+    for _ in range(5):
+        widget = QWidget()
+        ui = Ui_Form()
+        ui.setupUi(widget)
+        scroll_layout.addWidget(widget)
+    scroll_area = QScrollArea()
+    scroll_area.setWidget(scroll_widget)
+    scroll_area.setWidgetResizable(True)
+    scroll_area.setContentsMargins(0,0,0,0)
+    scroll_area.setStyleSheet(
+        """
+        QScrollArea {
+        border: none;
+        }
 
-    QScrollBar:vertical {
-        background: #f0f0f0;
-        width: 12px;
-    }
+        QScrollBar:vertical {
+            background: #f0f0f0;
+            width: 12px;
+        }
 
-    QScrollBar::handle:vertical {
-        background: #c0c0c0;
-        min-height: 30px;
-    }
+        QScrollBar::handle:vertical {
+            background: #c0c0c0;
+            min-height: 30px;
+        }
 
-    QScrollBar::add-line:vertical {
-        height: 20px;
-        subcontrol-position: bottom;
-    }
+        QScrollBar::add-line:vertical {
+            height: 20px;
+            subcontrol-position: bottom;
+        }
 
-    QScrollBar::sub-line:vertical {
-        height: 20px;
-        subcontrol-position: top;
-    }
-    """
-)
-main_widget.ui.centralLayout.addWidget(scroll_area)
-
-main_widget.show()
-
-app.exec()
+        QScrollBar::sub-line:vertical {
+            height: 20px;
+            subcontrol-position: top;
+        }
+        """
+    )
+    main_widget.ui.centralLayout.addWidget(scroll_area)
+    main_widget.show()
+    app.exec()
